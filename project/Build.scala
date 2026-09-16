3e44dc8db8bfef840ca87e0acf0368039266ab39
chaostone <duantihua@gmail.com>
2026-09-16 13:39:10 +0800

    reuse CasEntryPoint.serviceUrl in CasLocalEntryPoint
    - service url 复用 security 的 CasEntryPoint.serviceUrl，剔除 ticket/renew 等协议参数
    - sbt 插件加载时顺带升级依赖：security 4.5.4、commons 6.3.5、jdbc 1.1.17、cache 0.1.22、webmvc 0.15.3

--- project/Build.scala
+++ project/Build.scala
@@ -2,11 +2,11 @@ import org.beangle.parent.Dependencies.*
 import sbt.*
 
 object IdsDepends {
-  val b_commons = "org.beangle.commons" % "beangle-commons" % "6.3.2"
-  val b_jdbc = "org.beangle.jdbc" % "beangle-jdbc" % "1.1.14"
-  val b_cache = "org.beangle.cache" % "beangle-cache" % "0.1.21"
-  val b_security = "org.beangle.security" % "beangle-security" % "4.5.1"
-  val b_webmvc = "org.beangle.webmvc" % "beangle-webmvc" % "0.15.2"
+  val b_commons = "org.beangle.commons" % "beangle-commons" % "6.3.5"
+  val b_jdbc = "org.beangle.jdbc" % "beangle-jdbc" % "1.1.17"
+  val b_cache = "org.beangle.cache" % "beangle-cache" % "0.1.22"
+  val b_security = "org.beangle.security" % "beangle-security" % "4.5.4"
+  val b_webmvc = "org.beangle.webmvc" % "beangle-webmvc" % "0.15.3"
   val b_notify = "org.beangle.notify" % "beangle-notify" % "0.1.27"
 
   val coreDepends = Seq(logback_classic % "test", scalatest, b_commons, b_jdbc, b_cache, b_security)
--- src/main/scala/org/beangle/ids/cas/web/helper/CasLocalEntryPoint.scala
+++ src/main/scala/org/beangle/ids/cas/web/helper/CasLocalEntryPoint.scala
@@ -20,12 +20,11 @@ package org.beangle.ids.cas.web.helper
 import jakarta.servlet.http.HttpServletRequest
 import org.beangle.commons.lang.Strings
 import org.beangle.security.authc.AuthenticationException
-import org.beangle.security.realm.cas.CasConfig
+import org.beangle.security.realm.cas.{CasConfig, CasEntryPoint}
 import org.beangle.security.web.UrlEntryPoint
 import org.beangle.web.servlet.url.UrlBuilder
 
 import java.net.URLEncoder
-import java.util as ju
 
 class CasLocalEntryPoint(url: String) extends UrlEntryPoint(url) {
   /**
@@ -36,37 +35,13 @@ class CasLocalEntryPoint(url: String) extends UrlEntryPoint(url) {
     if (this.url.contains("${goto}")) {
       Strings.replace(this.url, "${goto}", UrlBuilder.url(req))
     } else {
-      val service = serviceUrl(req)
+      //service 与 CasPreauthFilter 校验时使用的保持一致，故复用 CasEntryPoint 的计算
+      val service = CasEntryPoint.serviceUrl(req)
       val sb = new StringBuilder(url)
       sb.append(if (url.indexOf("?") != -1) "&" else "?")
       sb.append(CasConfig.ServiceName + "=" + URLEncoder.encode(service, "UTF-8"))
       sb.toString()
     }
-  }
-
-  private def serviceUrl(req: HttpServletRequest): String = {
-    val buffer = new StringBuilder()
-    val serverName = CasConfig.getLocalServer(req)
-    buffer.append(serverName).append(req.getRequestURI)
-    val queryString = req.getQueryString
-    if (Strings.isNotBlank(queryString)) {
-      val parts = Strings.split(queryString, '&')
-      //这里的排序，保证请求和验证的使用的service是一样的
-      ju.Arrays.sort(parts.asInstanceOf[Array[AnyRef]])
-      val paramBuf = new StringBuilder
-      parts foreach { part =>
-        val equIdx = part.indexOf('=')
-        if (equIdx > 0) {
-          val key = part.substring(0, equIdx)
-          paramBuf.append("&").append(key).append(part.substring(equIdx))
-        }
-      }
-      if (paramBuf.nonEmpty) {
-        paramBuf.setCharAt(0, '?')
-        buffer.append(paramBuf)
-      }
-    }
-    buffer.toString
   }
 
 }
